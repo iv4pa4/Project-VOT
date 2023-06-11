707cc1245f044afbc5ee4bd3e1ea1c57d83e7936
Ivayla Panayotova <99187068+iv4pa4@users.noreply.github.com>
2023-06-11 23:15:19 +0300

    finally working quiz

--- quiz.php
+++ quiz.php
@@ -1,16 +1,13 @@
 <?php
-<<<<<<< Updated upstream
 
-=======
-// Assuming you have already established a database connection
->>>>>>> Stashed changes
 $db_host = '127.0.0.1';
 $db_user = 'root';
 $db_password = 'root';
 $db_db = 'quiz';
 $db_port = 8889;
 
-$mysqli = new mysqli(
+global $conn;
+$conn = new mysqli(
     $db_host,
     $db_user,
     $db_password,
@@ -18,59 +15,131 @@ $mysqli = new mysqli(
     $db_port
 );
 
-if ($mysqli->connect_error) {
-    echo 'Errno: '.$mysqli->connect_errno;
+if ($conn->connect_error) {
+    echo 'Errno: '.$conn->connect_errno;
     echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
+    echo 'Error: '.$conn->connect_error;
     exit();
 }
 
-<<<<<<< Updated upstream
-// Query the database to get 5 random questions
-$result = $mysqli->query("SELECT q.question, q.A, q.B, q.C, q.D, c.answer
-                         FROM questions AS q
-                         JOIN correct_answers AS c ON q.correct_answer_id = c.id
-                         ORDER BY RAND() LIMIT 5");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-$quizData = [];
-while ($row = $result->fetch_assoc()) {
-    $question = $row['question'];
-    $options = [$row['A'], $row['B'], $row['C'], $row['D']];
-    $correctAnswer = $row['answer'];
-=======
-// Retrieve questions and options from the database
-$query = 'SELECT question, correct_answer_id, A, B, C, D FROM questions';
-$result = $mysqli->query($query);
+// Fetch 5 random questions from the 'questions' table
+$query = "SELECT questions.*, correct_answers.answer FROM questions INNER JOIN correct_answers ON questions.correct_answer_id = correct_answers.id ORDER BY RAND() LIMIT 5";
+$result = mysqli_query($conn, $query);
 
 if (!$result) {
-    echo 'Error: '.$mysqli->error;
-    exit();
+    die("Error: " . mysqli_error($conn));
 }
 
-$quizData = [];
-
-while ($row = $result->fetch_assoc()) {
-    $question = $row['question'];
-    $correctAnswerId = $row['correct_answer_id'];
-    $options = [
-        $row['A'],
-        $row['B'],
-        $row['C'],
-        $row['D']
-    ];
->>>>>>> Stashed changes
-
-    $quizData[] = [
-        'question' => $question,
-        'options' => $options,
-<<<<<<< Updated upstream
-        'correctAnswer' => $correctAnswer
-=======
-        'correctAnswerId' => $correctAnswerId
->>>>>>> Stashed changes
-    ];
-}
-
-header('Content-Type: application/json');
-echo json_encode($quizData);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz</title>
+    <script src="script.js"></script>
+</head>
+<body>
+<h1>Quiz</h1>
+
+<div id="question-container">
+    <h3 id="question-text"></h3>
+    <div id="options-container">
+        <button class="option-btn" id="option-A"></button>
+        <button class="option-btn" id="option-B"></button>
+        <button class="option-btn" id="option-C"></button>
+        <button class="option-btn" id="option-D"></button>
+    </div>
+</div>
+
+<div id="result-container">
+    <h3 id="result-text"></h3>
+    <button id="next-btn" style="display: none;">Next</button>
+</div>
+
+<script>
+    // JavaScript code to handle quiz logic
+    var questions = <?php echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC)); ?>;
+    var currentQuestionIndex = 0;
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionTextElement = document.getElementById('question-text');
+    var optionAElement = document.getElementById('option-A');
+    var optionBElement = document.getElementById('option-B');
+    var optionCElement = document.getElementById('option-C');
+    var optionDElement = document.getElementById('option-D');
+    var resultTextElement = document.getElementById('result-text');
+    var nextButton = document.getElementById('next-btn');
+
+    function displayQuestion() {
+        var question = questions[currentQuestionIndex];
+        questionTextElement.innerText = question.question;
+        optionAElement.innerText = question.A;
+        optionBElement.innerText = question.B;
+        optionCElement.innerText = question.C;
+        optionDElement.innerText = question.D;
+    }
+
+    function checkAnswer(selectedOption) {
+        var question = questions[currentQuestionIndex];
+        var selectedOptionText = selectedOption.innerText;
+        var correctAnswer = question.answer;
+        var resultText = '';
+
+        if (selectedOptionText === correctAnswer) {
+            resultText = 'Correct!';
+        } else {
+            resultText = 'Wrong!';
+        }
+
+        resultTextElement.innerText = resultText;
+        nextButton.style.display = 'block';
+    }
+
+    function nextQuestion() {
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion();
+            resultTextElement.innerText = '';
+            nextButton.style.display = 'none';
+        } else {
+            // End of the quiz, display the final result
+            questionTextElement.innerText = 'Quiz finished!';
+            optionAElement.style.display = 'none';
+            optionBElement.style.display = 'none';
+            optionCElement.style.display = 'none';
+            optionDElement.style.display = 'none';
+            resultTextElement.innerText = 'You have completed the quiz.';
+            nextButton.style.display = 'none';
+        }
+    }
+
+    displayQuestion();
+
+    // Add event listeners to the option buttons
+    optionAElement.addEventListener('click', function() {
+        checkAnswer(optionAElement);
+    });
+
+    optionBElement.addEventListener('click', function() {
+        checkAnswer(optionBElement);
+    });
+
+    optionCElement.addEventListener('click', function() {
+        checkAnswer(optionCElement);
+    });
+
+    optionDElement.addEventListener('click', function() {
+        checkAnswer(optionDElement);
+    });
+
+    // Add event listener to the next button
+    nextButton.addEventListener('click', function() {
+        nextQuestion();
+    });
+</script>
+</body>
+</html>
